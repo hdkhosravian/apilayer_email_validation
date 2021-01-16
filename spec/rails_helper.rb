ed08228729cbb89e15831cc7800e4c80df93ad98
Hamid Khosravian <>
2021-01-16 11:59:59 +0330

    add rspec configs

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -7,6 +7,13 @@ require File.expand_path('../config/environment', __dir__)
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
+
+require 'database_cleaner'
+require 'factory_bot'
+
+require 'webmock/rspec'
+WebMock.disable_net_connect!(allow_localhost: true)
+
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
@@ -60,4 +67,5 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+  config.include Rails.application.routes.url_helpers
 end
